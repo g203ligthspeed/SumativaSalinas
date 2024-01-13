6339d4f215be987d08beb6e5900f3ab55873f9fc
g203ligthspeed <salinasrivasjeansebastian14@gmail.com>
2024-01-12 23:08:11 -0500

    Primer commit

---
+++ src/app/components/home/home.page.ts
@@ -0,0 +1,21 @@
+// home.page.ts
+
+import { Component } from '@angular/core';
+import { NewsCardComponent } from 'ruta-correcta/news-card/news-card.component';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {}
+// home.page.ts
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {}
